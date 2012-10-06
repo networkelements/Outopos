76c799c648ceef9ec48755b3f546cf86c495e60a
Lyrise <>
2012-10-06 17:47:42 +0900

    Lair
    バージョンアップ

--- Lair/Properties/AssemblyInfo.cs
+++ Lair/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.0.0.5857")] // eBrzTEEg0bVovASs5txXHdmQ0ZpyqjfX5bbPr5SCxoXKi+WnthsDNQ8NDHvyP5m9pWb1sPJBTTVsoN8cfnQmLg==
+[assembly: AssemblyVersion("0.0.0.5859")] // ms3Ekf8TRhK1ALDYVvGfAj2V3hcQXraNGZzMdpXG8F8ayHghYskHKTJ7a6mzjZUmyb4/+YUM+lNrUh6Bs2qoLQ==
