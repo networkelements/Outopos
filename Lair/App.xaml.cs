2b9852c0c34958aeed93310835e3c668a8734d81
Lyrise <>
2012-11-04 00:35:02 +0900

    Lair
    バージョンアップ

--- Lair/App.xaml.cs
+++ Lair/App.xaml.cs
@@ -37,7 +37,7 @@ namespace Lair
         {
             //System.Windows.Media.RenderOptions.ProcessRenderMode = System.Windows.Interop.RenderMode.SoftwareOnly;
 
-            App.LairVersion = new Version(0, 0, 31);
+            App.LairVersion = new Version(0, 0, 32);
 
             Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
 
